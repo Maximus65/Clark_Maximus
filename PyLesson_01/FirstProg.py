b35ccea621683cbc5b38ae1bacfad16b6b023fcf
Maximus65 <maximusclark1@gmail.com>
2016-09-02 09:59:07 -0700

    Added Files

---
+++ PyLesson_01/FirstProg.py
@@ -0,0 +1,34 @@
+print ("""
+
+                                       I
+                                      <I>
+                                     <III>
+                                    <IIIII>
+                                   <IIIIIII>
+                                  <IIIIIIIII>
+                                 <IIIIIIIIIII>
+                                <IIIIIIIIIIIII>
+                               <IIIIIIIIIIIIIII>
+                              <IIIIIIIIIIIIIIIII>
+                             <IIIIIIIIIIIIIIIIIII>
+                            <IIIIIIIIIIIIIIIIIIIII>
+                           <IIIIIIIIIIIIIIIIIIIIIII>
+                          <IIIIIIIIIIIIIIIIIIIIIIIII>
+                         <IIIIIIIIIIIIIIIIIIIIIIIIIII>
+                        <IIIIIIIIIIIIIIIIIIIIIIIIIIIII>
+                       <IIIIIIIIIIIIIIIIIIIIIIIIIIIIIII>
+                      <IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII>
+                                 IIIIIIIIIIIII
+                                 IIIIIIIIIIIII
+                                 IIIIIIIIIIIII
+                                IIIIIIIIIIIIIII
+                               IIIIIIIIIIIIIIIII
+_______________________________________________________________________________                               
+
+
+                                Christmas Tree
+
+
+
+                          
+""")
