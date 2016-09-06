a728e2eb917b955f2c34391d40350c740a58366d
Maximus65 <maximusclark1@gmail.com>
2016-09-06 10:15:38 -0700

    Added Lab_02.py, Notes, and Py_Lesson_02.py

---
+++ Py_Lesson_02/Lab_02.py
@@ -0,0 +1,18 @@
+l = 2
+W = 8
+a = 5
+b = 6
+area01 = l * W
+area02 = a * b
+print (" The area of the first shape is" , area01)
+print (" The area of the second shape is" , area02)
+adress = 12961
+stname = "Sea Breeze Farms Dr."
+city = "CA, San Diego"
+zipcode = 92130
+print (" I live on" , adress , stname , city , zipcode)
+Length = 16
+Height = 16
+Width = 16
+surfarea = Length * Height * Width
+print (" the surface area of the cube is" , surfarea)
---
+++ Py_Lesson_02/Py_Lesson_02.py
@@ -0,0 +1,4 @@
+l = 3
+W = 6
+RecArea = l * W
+print (" The area of your shape is" ,RecArea)
