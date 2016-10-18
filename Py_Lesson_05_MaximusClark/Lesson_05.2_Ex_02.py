04390a55b81c0b6f17924312388e1e909db4453c
Maximus65 <maximusclark1@gmail.com>
2016-10-18 10:06:43 -0700

    finished lesson_05.2

--- Py_Lesson_05_MaximusClark/Lesson_05.2_Ex_02.py
+++ Py_Lesson_05_MaximusClark/Lesson_05.2_Ex_02.py
@@ -7,12 +7,12 @@ pas = input("Please enter your Password: ")
 if(user == Username and pas == Password):
     print("access granted!")
 
-else:
-    if(user != Username and pas == Password):
-        print("Yor username is incorrect!")
-else:
-    if(user == Username and pas != Password):
-        print("Your password is incorrect!")
-else:
-    if(user != Username and pas != Password):
-        print("Your username and password is incorrect!") 
+if(user != Username and pas == Password):
+    print("Yor username is incorrect!")
+
+if(user == Username and pas != Password):
+    print("Your password is incorrect!")
+    
+if(user != Username and pas != Password):
+    print("Your username and password is incorrect!") 
+
