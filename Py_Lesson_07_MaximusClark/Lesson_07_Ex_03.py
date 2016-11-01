f535c8ec86209f453b6d0c85d6be1cea4062f2d1
Maximus65 <maximusclark1@gmail.com>
2016-11-01 09:51:17 -0700

    Finished Lesson_07_Ex_03 and Lesson_07_Ex_03

--- Py_Lesson_07_MaximusClark/Lesson_07_Ex_03.py
+++ Py_Lesson_07_MaximusClark/Lesson_07_Ex_03.py
@@ -1,1 +1,10 @@
-nuber = int(input("Please enter a number: ")) 
+number = int(input("Please enter a number: "))
+num = number
+rev = 0
+def Reverse(n):
+    while num > 0:
+        rev = rev*10
+        rev += 1
+        num = int(num / 10)
+        print("The number", number, "is being reversed: ", rev) 
+Reverse(number) 
---
+++ Py_Lesson_07_MaximusClark/Lesson_07_Ex_04.py
@@ -0,0 +1,5 @@
+sentence = input("Please enter a sentance: ")
+top = 0
+while top < sentence.count("a") > 0:
+    sentence = sentence[0 : sentence.index("a")] + sentence[sentence.index("a") + 1: len(sentence)]
+print("with out a's: " + sentence)     
